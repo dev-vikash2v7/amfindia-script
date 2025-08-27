ad6b935bb4f105d44ac7086b0b9272ad9ce7c82d
dev-vikash2v7 <vikashvermacom92@gmail.com>
2025-08-27 10:10:28 +0530

    'add_files'

---
+++ amfiindia.sh
@@ -0,0 +1,7 @@
+URL="https://www.amfiindia.com/spages/NAVAll.txt"
+OUT="amfi_nav.tsv"
+
+curl -s "$URL" | \
+awk -F ';' 'NR > 1 && NF >= 5 {print $4 "\t" $5}' > "$OUT"
+
+echo "Saved Scheme Name and Asset Value (NAV) into $OUT"

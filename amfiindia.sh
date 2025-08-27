9698f150d34c6372bbe32e07cc994f7db0ab91d3
dev-vikash2v7 <vikashvermacom92@gmail.com>
2025-08-27 10:18:04 +0530

    'update'

--- amfiindia.sh
+++ amfiindia.sh
@@ -4,4 +4,4 @@ OUT="amfi_nav.tsv"
 curl -s "$URL" | \
 awk -F ';' 'NR > 1 && NF >= 5 {print $4 "\t" $5}' > "$OUT"
 
-echo "Saved Scheme Name and Asset Value (NAV) into $OUT"
+echo "Saved Output into $OUT"
